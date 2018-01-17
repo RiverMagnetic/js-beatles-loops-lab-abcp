be70caaef50a702a3d5fbdcd6f09da3a463fd932
River Magnetic <magneticcontinuum@gmail.com>
2018-01-17 01:30:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function theBeatlesPlay (musicians, instruments) {
+  var whoPlaysWhat = [] 
+  for (var i = 0; i < 4; i++) {
+  whoPlaysWhat.push((musicians[i]) +" plays " + (instruments[i]))
+}
+return whoPlaysWhat
+}
+
+function johnLennonFacts (facts) {
+  
+}
