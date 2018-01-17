3e8aa8c6f932fda25c5025f3032c43e2b239fb80
River Magnetic <magneticcontinuum@gmail.com>
2018-01-17 15:07:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,8 @@ const facts = [
 function johnLennonFacts (facts) {
   var i = 0
   var loudFacts = []
-  while (i < 3) {
-  (i++)
+  
   loudFacts.push((facts[i]) + "!!!")
-}
+
 return loudFacts;
 }
