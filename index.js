e9baa2042bbae20561f76ea6b5cad5d9ea4f9973
River Magnetic <magneticcontinuum@gmail.com>
2018-01-17 02:10:52 +0000

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ function theBeatlesPlay (musicians, instruments) {
 return whoPlaysWhat
 }
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+]
+
 function johnLennonFacts (facts) {
-  
+  var i = 0
+  while (i < 4) {
+  (i++)
+  var loudFacts = ((facts[i]) + "!!!")
+}
+return loudFacts
 }
