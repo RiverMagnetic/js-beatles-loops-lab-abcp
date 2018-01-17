bde09f9f5c293b052b9f34a11f88561cde31618e
River Magnetic <magneticcontinuum@gmail.com>
2018-01-17 15:58:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function theBeatlesPlay (musicians, instruments) {
+function theBeatlesPlay(musicians, instruments) {
   var whoPlaysWhat = [] 
   for (var i = 0; i < 4; i++) {
   whoPlaysWhat.push((musicians[i]) +" plays " + (instruments[i]))
@@ -6,20 +6,22 @@ function theBeatlesPlay (musicians, instruments) {
 return whoPlaysWhat
 }
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-]
-
-function johnLennonFacts (facts) {
-  var i = -1
+function johnLennonFacts(facts) {
+  var i = 0
   var loudFacts = []
-  while (i < 3) {
-    (i++)
+  while (i < facts.length) {
   loudFacts.push((facts[i]) + "!!!")
-  
+  i++
 }
-return loudFacts;
+return loudFacts
+}
+
+function iLoveTheBeatles (n){
+  {
+  var loveBeatles = []
+  do {
+    loveBeatles.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+} return loveBeatles
 }
